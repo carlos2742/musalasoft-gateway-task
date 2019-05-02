d125696558668521eb11c95aa1e33d846841747d
Carlos <carlosm@yzag.com>
2019-05-02 10:38:11 -0400

    refactoring

--- src/app/components/form-dev/form-dev.component.ts
+++ src/app/components/form-dev/form-dev.component.ts
@@ -75,7 +75,7 @@ export class FormDevComponent implements OnInit {
 
   private createForm(uid = '', vendor = '', status = DEVICES_STATUS.ONLINE) {
     this.customform = this._formBuilder.group({
-      'uid': new FormControl(uid, [Validators.required]),
+      'uid': new FormControl(uid, [Validators.required, Validators.pattern('(?<=\\s|^)\\d+(?=\\s|$)')]),
       'vendor': new FormControl(vendor, [Validators.required]),
       'status': new FormControl(status),
     });
